26c51376db2e2e029c9a68cd7b18b320269fc2c2
Bektemur Mamashaev <104037405+bektemur-07@users.noreply.github.com>
2023-02-02 13:12:01 +0500

    work with ViewController

---
+++ Quizlet/Controller/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Quizlet
+//
+//  Created by Bektemur on 02/02/23.
+//
+
+import UIKit
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var falseButton: UIButton!
+    @IBOutlet var trueButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        progressBar.progress = 0
+    }
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+}
+
